9d75908b7fb2cb663a0333623fdcddc3e8aa0ba1
Sabrina Tjeng <sabriele@users.noreply.github.com>
2019-02-13 13:58:40 +0800

    Create to-do list form

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
-const tasks = [
-	'buy milk',
-	'eat dinner',
-	'nail javascript',
-	'give feedback',
-];
+const tasks = ['buy milk', 'eat dinner', 'nail javascript', 'give feedback'];
+const allTasks = document.querySelector('#todo-list');
+
+const removeButton = () => {
+  const button = document.createElement('button');
+  button.textContent = 'Remove';
+  return button;
+};
+
+const toggleDone = () => {
+  const task = document.querySelectorAll('li');
+  task.forEach((node) => {
+    node.addEventListener('click', () => {
+      node.classList.toggle('done');
+    });
+  });
+};
+
+const displayTasks = () => {
+  tasks.forEach((task) => {
+    const newTask = document.createElement('li');
+    newTask.textContent = task;
+    allTasks.appendChild(newTask);
+    toggleDone();
+    removeButton();
+  });
+};
+
+const addTask = () => {
+  const taskSubmit = document.querySelector('#submission');
+  const addButton = document.querySelector('#addButton');
+
+  addButton.addEventListener('click', () => {
+    const ul = document.querySelector('#todo-list');
+    while (ul.hasChildNodes()) {
+      ul.lastChild.remove();
+    }
+    tasks.push(taskSubmit.value);
+    displayTasks();
+  });
+
+  taskSubmit.addEventListener('keypress', (e) => {
+    const key = e.which || e.keyCode;
+    if (key === 13) {
+      const newTask = document.createElement('li');
+      newTask.textContent = taskSubmit.value;
+      allTasks.appendChild(newTask);
+      tasks.push(newTask.textContent);
+    }
+  });
+};
+
+displayTasks();
+addTask();
+toggleDone();
